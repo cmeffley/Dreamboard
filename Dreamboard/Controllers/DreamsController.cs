02bddb7da244ba798f1d499a0ff811824c2d4f8c
Chris Meffley <cmeffley@gmail.com>
2022-03-01 14:29:14 -0700

    Add project files.

---
+++ Dreamboard/Controllers/DreamsController.cs
@@ -0,0 +1,29 @@
+﻿using Dreamboard.DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dreamboard.Controllers
+{
+    [Route("dreams")]
+    [ApiController]
+    public class DreamsController : ControllerBase
+    {
+        DreamsRepository _repo;
+
+        public DreamsController(DreamsRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllDreams()
+        {
+           return Ok(_repo.GetAll());
+        }
+
+    }
+}
---
+++ Dreamboard/DataAccess/DreamsRepository.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using Dreamboard.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dreamboard.DataAccess
+{
+    public class DreamsRepository
+    {
+        readonly string _connectionString;
+
+        public DreamsRepository(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("Dreamboard");
+        }
+
+        internal IEnumerable<Dreams> GetAll()
+        {
+            using var db = new SqlConnection(_connectionString);
+
+            var dreams = db.Query<Dreams>(@"Select * From Dreams");
+
+            return dreams;
+        }
+    }
+}
